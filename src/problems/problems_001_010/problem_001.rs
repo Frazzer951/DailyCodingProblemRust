5807527d49dffe8a3cab7ee545d0f137314f1d4f
Luke Eltiste <luke343279@gmail.com>
2022-02-07 14:47:31 -0800

    problem_001.rs

--- src/problems/problems_001_010/problem_001.rs
+++ src/problems/problems_001_010/problem_001.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* EASY
 Given a list of numbers and a number k, return whether any two numbers from the
 list add up to k.
@@ -9,8 +7,16 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 Bonus: Can you do this in one pass?
 */
 
-fn problem_001() -> i64 {
-    0
+fn problem_001(arr: Vec<i64>, k: i64) -> bool {
+    for i in 0..arr.len() {
+        let x = arr[i];
+        let j = (k - x).abs();
+        if arr.contains(&j) {
+            return true;
+        }
+    }
+
+    false
 }
 
 #[cfg(test)]
@@ -19,6 +25,7 @@ mod tests {
 
     #[test]
     fn test_problem_001() {
-        assert_eq!(problem_001(), 1);
+        assert_eq!(problem_001(vec! {10,15,3,7}, 17), true);
+        assert_eq!(problem_001(vec! {10,15,3,7}, 16), false);
     }
 }
