878cfb5251080879d6daa756d7aeb70353573081
Luke Eltiste <luke343279@gmail.com>
2022-02-07 14:28:45 -0800

    Autogen README.md

---
+++ scripts/generateReadme.py
@@ -0,0 +1,118 @@
+import logging
+import os
+import re
+from typing import Dict
+from typing import List
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+
+def _replace_between_tags(string: str, content: str, start: str, end: str) -> str:
+    content = "\n".join([start, content, end])
+
+    return re.sub(
+        pattern=rf"{start}.*?{end}",
+        repl=content,
+        string=string,
+        flags=re.DOTALL,
+    )
+
+
+def get_problems() -> Dict[str, List[str]]:
+    problems = {
+        "Completed": [],
+        "Incomplete": [],
+        "Skipped": [],
+    }
+    path = os.path.join(ROOT_DIR, "../src/problems")
+    problems_folder = os.path.abspath(path)
+
+    sub_folders = [x[0] for x in os.walk(problems_folder)]
+
+    for folder in sub_folders:
+        if folder == problems_folder:
+            continue
+        logging.debug(f"Processing {folder}")
+        files = os.listdir(folder)
+        for file in files:
+            logging.debug(file)
+            with open(os.path.join(folder, file), encoding="UTF-8") as f:
+                content = f.read()
+                if "// NOT DONE" in content:
+                    problems["Incomplete"].append(file)
+                elif "// SKIPPED" in content:
+                    problems["Skipped"].append(file)
+                else:
+                    problems["Completed"].append(file)
+
+    return problems
+
+
+def create_readme_links(files: List[str]) -> List[str]:
+    text: List[str] = []
+
+    for file in files:
+        num = int(file.rstrip(".rs").split("_")[-1])
+        section = (num - 1) // 10
+        text.append(f" - [Problem {num:03}](src/problems/problems_{section:02}1_{section + 1:02}0/{file})")
+
+    return text
+
+
+def create_completed_text(problems: List[str]) -> str:
+    text = ["## Completed ⭐️", "<details><summary>Completed</summary>", "<p>", ""]
+    text += create_readme_links(problems)
+    text += ["", "</p>", "</details>", ""]
+
+    return "\n".join(text)
+
+
+def create_missing_text(problems: List[str]) -> str:
+    text = ["## Missing ❌️", "<details><summary>Missing</summary>", "<p>", ""]
+    text += create_readme_links(problems)
+    text += ["", "</p>", "</details>", ""]
+
+    return "\n".join(text)
+
+
+def create_skipped_text(problems: List[str]) -> str:
+    text = ["## Skipped️", "<details><summary>Skipped</summary>", "<p>", ""]
+    text += create_readme_links(problems)
+    text += ["", "</p>", "</details>", ""]
+
+    return "\n".join(text)
+
+
+def generate_readme():
+    path = os.path.join(ROOT_DIR, "../README.md")
+    readme_file = os.path.abspath(path)
+
+    problems = get_problems()
+
+    with open(readme_file, encoding="UTF-8") as f:
+        current_readme = f.read()
+
+    readme = _replace_between_tags(
+        current_readme, create_completed_text(problems['Completed']), "<!-- start completed section -->",
+        "<!-- end completed section -->"
+    )
+
+    readme = _replace_between_tags(
+        readme, create_missing_text(problems['Incomplete']), "<!-- start missing section -->",
+        "<!-- end missing section -->"
+    )
+
+    readme = _replace_between_tags(
+        readme, create_skipped_text(problems['Skipped']), "<!-- start skipped section -->",
+        "<!-- end skipped section -->"
+    )
+
+    with open(readme_file, "w", encoding="UTF-8") as f:
+        f.write(readme)
+
+
+if __name__ == "__main__":
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    generate_readme()
