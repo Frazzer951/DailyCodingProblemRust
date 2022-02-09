3ce25d2e4134658760550640e20ca06f54fd7a5d
Luke Eltiste <luke343279@gmail.com>
2022-02-09 15:33:50 -0800

    problem_023.rs

--- src/problems/problems_021_030/problem_023.rs
+++ src/problems/problems_021_030/problem_023.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* EASY
 You are given an M by N matrix consisting of booleans that represents a board.
 Each True boolean represents a wall. Each False boolean represents a tile you
@@ -23,7 +21,48 @@ of steps required to reach the end is 7, since we would need to go through (1,
 2) because there is a wall everywhere else on the second row.
 */
 
-fn problem_023() -> i64 {
+use std::collections::{hash_set::HashSet, VecDeque};
+
+#[derive(PartialEq, Eq, Hash, Clone)]
+struct Point(i64, i64);
+
+fn get_walkable_neighbors(board: &Vec<Vec<bool>>, point: &Point) -> Vec<Point> {
+    let row = point.0;
+    let col = point.1;
+    let mut neighbors = Vec::new();
+    if row > 0 && !board[row as usize - 1][col as usize] {
+        neighbors.push(Point(row - 1, col));
+    }
+    if col > 0 && !board[row as usize][col as usize - 1] {
+        neighbors.push(Point(row, col - 1));
+    }
+    if row < board.len() as i64 - 1 && !board[row as usize + 1][col as usize] {
+        neighbors.push(Point(row + 1, col));
+    }
+    if col < board[0].len() as i64 - 1 && !board[row as usize][col as usize + 1] {
+        neighbors.push(Point(row, col + 1));
+    }
+    neighbors
+}
+
+fn shortest_path(board: Vec<Vec<bool>>, start: Point, end: Point) -> i64 {
+    let mut seen = HashSet::new();
+    let mut queue = VecDeque::from([(start, 0)]);
+
+    while !queue.is_empty() {
+        let (coords, count) = queue.pop_front().unwrap();
+        if coords == end {
+            return count;
+        }
+        seen.insert(coords.clone());
+        let neighbors = get_walkable_neighbors(&board, &coords);
+        for neighbor in neighbors {
+            if !seen.contains(&neighbor) {
+                queue.push_back((neighbor, count + 1));
+            }
+        }
+    }
+
     0
 }
 
@@ -34,6 +73,18 @@ mod tests {
     #[test]
     #[ignore]
     fn test_problem_023() {
-        assert_eq!(problem_023(), 1);
+        assert_eq!(
+            shortest_path(
+                vec![
+                    vec![false, false, false, false],
+                    vec![true, true, false, true],
+                    vec![false, false, false, false],
+                    vec![false, false, false, false],
+                ],
+                Point(3, 0),
+                Point(0, 0),
+            ),
+            7
+        );
     }
 }
