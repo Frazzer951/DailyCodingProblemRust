636a7a432af15e59452b49f8b3910416b1995b8e
Luke Eltiste <luke343279@gmail.com>
2022-02-09 09:03:39 -0800

    problem_008.rs

--- src/problems/problems_001_010/problem_008.rs
+++ src/problems/problems_001_010/problem_008.rs
@@ -17,8 +17,28 @@ For example, the following tree has 5 unival subtrees:
  1   1
 */
 
-fn problem_008() -> i64 {
-    0
+use crate::utils::bt_node::BtNode;
+
+fn same_children<T: std::cmp::PartialEq>(root: &BtNode<T>) -> bool {
+    if root.l.is_none() && root.r.is_none() { return true; }
+
+    if !root.l.is_none() && root.val != root.l.as_ref().unwrap().val { return false; }
+    if !root.r.is_none() && root.val != root.r.as_ref().unwrap().val { return false; }
+
+    let left = if !root.l.is_none() { same_children(&*root.l.as_ref().unwrap()) } else { true };
+    let right = if !root.r.is_none() { same_children(&*root.r.as_ref().unwrap()) } else { true };
+
+    return left && right;
+}
+
+fn count_unival_tree<T: std::cmp::PartialEq>(root: &BtNode<T>) -> i64 {
+    if root.l.is_none() && root.r.is_none() { return 1; }
+    let mut count = 0;
+
+    if same_children(root) { count += 1; }
+    let left = if !root.l.is_none() { count_unival_tree(&*root.l.as_ref().unwrap()) } else { 0 };
+    let right = if !root.r.is_none() { count_unival_tree(&*root.r.as_ref().unwrap()) } else { 0 };
+    return count + left + right;
 }
 
 #[cfg(test)]
@@ -26,8 +46,45 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_problem_008() {
-        assert_eq!(problem_008(), 1);
+        /*
+           0
+          / \
+         1   0
+            / \
+           1   0
+          / \
+         1   1
+         */
+        let node = BtNode {
+            val: 0,
+            l: Some(Box::new(BtNode {
+                val: 1,
+                l: None,
+                r: None,
+            })),
+            r: Some(Box::new(BtNode {
+                val: 0,
+                l: Some(Box::new(BtNode {
+                    val: 1,
+                    l: Some(Box::new(BtNode {
+                        val: 1,
+                        l: None,
+                        r: None,
+                    })),
+                    r: Some(Box::new(BtNode {
+                        val: 1,
+                        l: None,
+                        r: None,
+                    })),
+                })),
+                r: Some(Box::new(BtNode {
+                    val: 0,
+                    l: None,
+                    r: None,
+                })),
+            })),
+        };
+        assert_eq!(count_unival_tree(&node), 5);
     }
 }
