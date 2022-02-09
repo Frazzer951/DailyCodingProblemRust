19abe013e7c07ce9d7bb06d675ec77ca19c78377
Luke Eltiste <luke343279@gmail.com>
2022-02-09 08:45:38 -0800

    problem_007.rs

--- src/problems/problems_001_010/problem_007.rs
+++ src/problems/problems_001_010/problem_007.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* MEDIUM
 Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the
 number of ways it can be decoded.
@@ -11,8 +9,25 @@ You can assume that the messages are decodable. For example, '001' is not
 allowed.
 */
 
-fn problem_007() -> i64 {
-    0
+fn problem_007(s: &str) -> i64 {
+    if s.len() == 0 {
+        return 1;
+    }
+    if s.chars().next().unwrap() == '0' {
+        return 0;
+    }
+    if s.len() == 1 {
+        return 1;
+    }
+
+    let mut total = 0;
+    let x: i64 = s[0..2].parse().unwrap();
+    if x <= 26 {
+        total += problem_007(&s[2..]);
+    }
+
+    total += problem_007(&s[1..]);
+    total
 }
 
 #[cfg(test)]
@@ -20,8 +35,8 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_problem_007() {
-        assert_eq!(problem_007(), 1);
+        assert_eq!(problem_007("111"), 3);
+        assert_eq!(problem_007("001"), 0);
     }
 }
