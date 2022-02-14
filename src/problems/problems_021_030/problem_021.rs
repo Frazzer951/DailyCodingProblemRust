81291643f2a3e34ce121c05849ae6c4a2132e6cf
Luke Eltiste <luke343279@gmail.com>
2022-02-14 08:28:42 -0800

    problem_021.rs

--- src/problems/problems_021_030/problem_021.rs
+++ src/problems/problems_021_030/problem_021.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* EASY
 Given an array of time intervals (start, end) for classroom lectures (possibly
 overlapping), find the minimum number of rooms required.
@@ -7,8 +5,25 @@ overlapping), find the minimum number of rooms required.
 For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
 */
 
-fn problem_021() -> i64 {
-    0
+fn problem_021(schedule: Vec<(u64, u64)>) -> u64 {
+    if schedule.len() == 0 {
+        return 0;
+    }
+    let mut conflicts = vec![];
+
+    let cur_class = schedule[0];
+
+    for i in 1..schedule.len() {
+        let next_class = schedule[i];
+        if (cur_class.1 >= next_class.0 && cur_class.0 < next_class.1)
+            || (cur_class.1 > next_class.0 && cur_class.1 <= next_class.1)
+            || (cur_class.0 < next_class.1 && cur_class.0 > next_class.0)
+        {
+            conflicts.push(schedule[i]);
+        }
+    }
+
+    1 + problem_021(conflicts)
 }
 
 #[cfg(test)]
@@ -16,8 +31,7 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_problem_021() {
-        assert_eq!(problem_021(), 1);
+        assert_eq!(problem_021(vec![(30, 75), (0, 50), (60, 150)]), 2);
     }
 }
