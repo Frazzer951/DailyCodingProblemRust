a991bf98e0e1dab5f5f72b2cb55df66dc9bb825c
Luke Eltiste <luke343279@gmail.com>
2022-02-14 08:45:27 -0800

    problem_022.rs

--- src/problems/problems_021_030/problem_022.rs
+++ src/problems/problems_021_030/problem_022.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* MEDIUM
 Given a dictionary of words and a string made up of those words (no spaces),
 return the original sentence in a list. If there is more than one possible
@@ -14,8 +12,25 @@ Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the string
 'and', 'beyond'].
 */
 
-fn problem_022() -> i64 {
-    0
+fn problem_022(word_set: Vec<String>, mut str: String) -> Vec<String> {
+    let mut sentance = Vec::new();
+
+    while str.len() > 0 {
+        let mut word_found = false;
+        for word in &word_set {
+            let index = str.find(&word.to_string()).unwrap_or(str.len());
+            if index == 0 {
+                sentance.push(word.clone().to_string());
+                str.replace_range(0..word.len(), "");
+                word_found = true;
+            }
+        }
+        if !word_found {
+            break;
+        }
+    }
+
+    sentance
 }
 
 #[cfg(test)]
@@ -23,8 +38,41 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_problem_022() {
-        assert_eq!(problem_022(), 1);
+        assert_eq!(
+            problem_022(
+                vec![
+                    String::from("quick"),
+                    String::from("brown"),
+                    String::from("the"),
+                    String::from("fox")
+                ],
+                String::from("thequickbrownfox")
+            ),
+            vec![
+                String::from("the"),
+                String::from("quick"),
+                String::from("brown"),
+                String::from("fox")
+            ]
+        );
+        assert_eq!(
+            problem_022(
+                vec![
+                    String::from("bed"),
+                    String::from("bath"),
+                    String::from("bedbath"),
+                    String::from("and"),
+                    String::from("beyond")
+                ],
+                String::from("bedbathandbeyond")
+            ),
+            vec![
+                String::from("bed"),
+                String::from("bath"),
+                String::from("and"),
+                String::from("beyond")
+            ]
+        );
     }
 }
