90fea8922fb3b52b53cf759235782a021093b5d9
Luke Eltiste <luke343279@gmail.com>
2022-02-16 10:56:04 -0800

    problem_035.rs

--- src/problems/problems_031_040/problem_035.rs
+++ src/problems/problems_031_040/problem_035.rs
@@ -11,8 +11,41 @@ For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should
 become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
 */
 
-fn problem_035() -> i64 {
-    0
+fn problem_035(arr: &mut Vec<char>) {
+    let mut r = 0;
+    let mut g = 0;
+    let mut b = 0;
+
+    for c in arr.iter() {
+        match c {
+            'R' => r += 1,
+            'G' => g += 1,
+            'B' => b += 1,
+            _ => {
+                panic!("{} isn't R, G, or B", c)
+            }
+        }
+    }
+    g += r;
+    b += g;
+
+    for i in 0..arr.len() {
+        match arr[i] {
+            'R' => {
+                arr.swap(i, r - 1);
+                r -= 1;
+            }
+            'G' => {
+                arr.swap(i, g - 1);
+                g -= 1;
+            }
+            'B' => {
+                arr.swap(i, b - 1);
+                b -= 1;
+            }
+            _ => panic!("How did you get here?"),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -20,8 +53,9 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_problem_035() {
-        assert_eq!(problem_035(), 1);
+        let mut vec = vec!['G', 'B', 'R', 'R', 'B', 'R', 'G'];
+        problem_035(&mut vec);
+        assert_eq!(vec, vec!['R', 'R', 'R', 'G', 'G', 'B', 'B']);
     }
 }
