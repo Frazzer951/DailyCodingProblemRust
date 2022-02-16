7b79e11af2ae412ed053a02279a0d89a1db0c7e9
Luke Eltiste <luke343279@gmail.com>
2022-02-16 08:38:23 -0800

    problem_031.rs

--- src/problems/problems_031_040/problem_031.rs
+++ src/problems/problems_031_040/problem_031.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* EASY
 The edit distance between two strings refers to the minimum number of character
 insertions, deletions, and substitutions required to change one string to the
@@ -9,8 +7,31 @@ substitute the “k” for “s”, substitute the “e” for “i”, and appe
 Given two strings, compute the edit distance between them.
 */
 
-fn problem_031() -> i64 {
-    0
+use std::cmp::min;
+
+fn problem_031(s1: &str, s2: &str) -> i64 {
+    let x = s1.len() + 1;
+    let y = s2.len() + 1;
+    let mut a = vec![vec![-1; x]; y];
+
+    for i in 0..x {
+        a[0][i] = i as i64;
+    }
+    for (j, arr) in a.iter_mut().enumerate().take(y) {
+        arr[0] = j as i64;
+    }
+
+    for i in 1..y {
+        for j in 1..x {
+            if s1.chars().nth(j - 1).unwrap() == s2.chars().nth(i - 1).unwrap() {
+                a[i][j] = a[i - 1][j - 1];
+            } else {
+                a[i][j] = min(min(a[i - 1][j] + 1, a[i][j - 1] + 1), a[i - 1][j - 1] + 1);
+            }
+        }
+    }
+
+    a[y - 1][x - 1]
 }
 
 #[cfg(test)]
@@ -20,6 +41,6 @@ mod tests {
     #[test]
     #[ignore]
     fn test_problem_031() {
-        assert_eq!(problem_031(), 1);
+        assert_eq!(problem_031("kitten", "sitting"), 3);
     }
 }
