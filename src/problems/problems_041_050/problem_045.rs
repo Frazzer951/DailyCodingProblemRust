594194c050597b1c2f83a4c709b3bed073ea009e
Luke Eltiste <luke343279@gmail.com>
2022-02-16 12:07:43 -0800

    problem_045.rs

--- src/problems/problems_041_050/problem_045.rs
+++ src/problems/problems_041_050/problem_045.rs
@@ -1,13 +1,32 @@
-// NOT DONE
-
 /* EASY
 Using a function rand5() that returns an integer from 1 to 5 (inclusive) with
 uniform probability, implement a function rand7() that returns an integer from 1
 to 7 (inclusive).
 */
 
-fn problem_045() -> i64 {
-    0
+use rand::Rng;
+
+fn rand5() -> i64 {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(0..=5)
+}
+
+fn rand7() -> i64 {
+    let r1 = rand5();
+    let r2 = rand5();
+    if r2 <= 3 {
+        return r1;
+    }
+    if r2 == 4 {
+        if r1 <= 3 {
+            return 6;
+        }
+        return rand7();
+    }
+    if r1 <= 3 {
+        return 7;
+    }
+    rand7()
 }
 
 #[cfg(test)]
@@ -15,8 +34,8 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_problem_045() {
-        assert_eq!(problem_045(), 1);
+        let val = rand7();
+        assert!(val <= 7 || val >= 1);
     }
 }
