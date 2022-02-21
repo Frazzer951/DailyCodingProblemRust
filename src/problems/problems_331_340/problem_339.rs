2f02586c5e564e1faa1f114c958513c882a3e7c1
Luke Eltiste <luke343279@gmail.com>
2022-02-21 15:11:57 -0800

    problem_339.rs

--- src/problems/problems_331_340/problem_339.rs
+++ src/problems/problems_331_340/problem_339.rs
@@ -1,13 +1,21 @@
-// NOT DONE
-
 /* EASY
 Given an array of numbers and a number k, determine if there are three entries
 in the array which add up to the specified number k. For example, given [20,
 303, 3, 4, 25] and k = 49, returntrue as 20 + 4 + 25 = 49.
 */
 
-fn problem_339() -> i64 {
-    0
+fn problem_339(arr: Vec<i64>, k: i64) -> bool {
+    for i in 0..arr.len() {
+        for j in i..arr.len() {
+            for l in j..arr.len() {
+                if arr[i] + arr[j] + arr[l] == k {
+                    return true;
+                }
+            }
+        }
+    }
+
+    false
 }
 
 #[cfg(test)]
@@ -15,8 +23,9 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_problem_339() {
-        assert_eq!(problem_339(), 1);
+        assert!(problem_339(vec![20, 303, 3, 4, 25], 48));
+        assert!(problem_339(vec![20, 303, 3, 4, 25], 49));
+        assert!(!problem_339(vec![20, 303, 3, 4, 25], 50));
     }
 }
