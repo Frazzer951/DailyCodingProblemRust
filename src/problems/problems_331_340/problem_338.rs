1f45bbe590bfa1cca537a4eb7c68ef7f499a60da
Luke Eltiste <luke343279@gmail.com>
2022-02-21 15:25:36 -0800

    problem_338.rs

--- src/problems/problems_331_340/problem_338.rs
+++ src/problems/problems_331_340/problem_338.rs
@@ -1,12 +1,29 @@
-// NOT DONE
-
 /* MEDIUM
 Given an integer n, find the next biggest integer with the same number of 1-bits
 on. For example, given the number 6 (0110 in binary), return 9 (1001).
 */
 
-fn problem_338() -> i64 {
-    0
+fn problem_338(mut num: i64) -> i64 {
+    if num == 0 {
+        return 0;
+    }
+
+    let mut count = 0;
+    let mut mask = num & !(num - 1);
+    while !((num & mask) > 0 && (num & (mask << 1)) == 0) {
+        num &= !mask;
+        count += 1;
+        mask = num & !(num - 1);
+    }
+
+    num &= !mask;
+    num |= mask << 1;
+
+    for i in 0..count {
+        num |= 1 << i;
+    }
+
+    num
 }
 
 #[cfg(test)]
@@ -14,8 +31,7 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_problem_338() {
-        assert_eq!(problem_338(), 1);
+        assert_eq!(problem_338(6), 9);
     }
 }
