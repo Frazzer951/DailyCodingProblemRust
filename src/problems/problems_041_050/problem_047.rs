30d8534dd3a0ff94ecac8bbefcd86e2cac9e27a5
Luke Eltiste <luke343279@gmail.com>
2022-02-21 08:18:39 -0800

    problem_047.rs

--- src/problems/problems_041_050/problem_047.rs
+++ src/problems/problems_041_050/problem_047.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* EASY
 Given a array of numbers representing the stock prices of a company in
 chronological order, write a function that calculates the maximum profit you
@@ -10,8 +8,23 @@ For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could
 buy the stock at 5 dollars and sell it at 10 dollars.
 */
 
-fn problem_047() -> i64 {
-    0
+fn problem_047(prices: Vec<i64>) -> i64 {
+    let mut min = prices[0];
+    let mut max = prices[0];
+    let mut max_profit = 0;
+
+    for x in prices {
+        if x < min {
+            min = x;
+            max = x;
+        }
+        if x > max {
+            max = x;
+            max_profit = max_profit.max(max - min);
+        }
+    }
+
+    max_profit
 }
 
 #[cfg(test)]
@@ -19,8 +32,8 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
+
     fn test_problem_047() {
-        assert_eq!(problem_047(), 1);
+        assert_eq!(problem_047(vec![9, 11, 8, 5, 7, 10]), 5);
     }
 }
