0dab1a038c4bb6723065286bb3f1884033987e2f
Luke Eltiste <luke343279@gmail.com>
2022-02-21 13:32:32 -0800

    problem_063.rs

--- src/problems/problems_061_070/problem_063.rs
+++ src/problems/problems_061_070/problem_063.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* EASY
 Given a 2D matrix of characters and a target word, write a function that returns
 whether the word can be found in the matrix by going left-to-right, or
@@ -18,8 +16,48 @@ column. Similarly, given the target word 'MASS', you should return true, since
 it's the last row.
 */
 
-fn problem_063() -> i64 {
-    0
+fn check_word_right(arr: &[Vec<char>], r: usize, c: usize, word: &str) -> bool {
+    let word_len = word.len();
+    let row_len = arr[0].len();
+    if word_len != row_len - c {
+        return false;
+    }
+    for i in 0..word_len {
+        let cur_char = word.chars().nth(i).unwrap();
+        if cur_char != arr[r][i + c] {
+            return false;
+        }
+    }
+    true
+}
+
+fn check_word_down(arr: &[Vec<char>], r: usize, c: usize, word: &str) -> bool {
+    let word_len = word.len();
+    let col_len = arr.len();
+    if word_len != col_len - r {
+        return false;
+    }
+    for i in 0..word_len {
+        let cur_char = word.chars().nth(i).unwrap();
+        if cur_char != arr[i + r][c] {
+            return false;
+        }
+    }
+    true
+}
+
+fn problem_063(arr: Vec<Vec<char>>, word: &str) -> bool {
+    for r in 0..arr.len() {
+        for c in 0..arr[r].len() {
+            if check_word_right(&arr, r, c, word) {
+                return true;
+            }
+            if check_word_down(&arr, r, c, word) {
+                return true;
+            }
+        }
+    }
+    false
 }
 
 #[cfg(test)]
@@ -27,8 +65,33 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_problem_063() {
-        assert_eq!(problem_063(), 1);
+        assert!(problem_063(
+            vec![
+                vec!['F', 'A', 'C', 'I'],
+                vec!['O', 'B', 'Q', 'P'],
+                vec!['A', 'N', 'O', 'B'],
+                vec!['M', 'A', 'S', 'S']
+            ],
+            "FOAM"
+        ));
+        assert!(problem_063(
+            vec![
+                vec!['F', 'A', 'C', 'I'],
+                vec!['O', 'B', 'Q', 'P'],
+                vec!['A', 'N', 'O', 'B'],
+                vec!['M', 'A', 'S', 'S']
+            ],
+            "MASS"
+        ));
+        assert!(!problem_063(
+            vec![
+                vec!['F', 'A', 'C', 'I'],
+                vec!['O', 'B', 'Q', 'P'],
+                vec!['A', 'N', 'O', 'B'],
+                vec!['M', 'A', 'S', 'S']
+            ],
+            "CARS"
+        ));
     }
 }
