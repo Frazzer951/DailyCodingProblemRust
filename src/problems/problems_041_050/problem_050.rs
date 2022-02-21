8d5d819dcdee588cb98c194dc1c8f8fe3094af76
Luke Eltiste <luke343279@gmail.com>
2022-02-21 08:28:19 -0800

    problem_050.rs

--- src/problems/problems_041_050/problem_050.rs
+++ src/problems/problems_041_050/problem_050.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* EASY
 Suppose an arithmetic expression is given as a binary tree. Each leaf is an
 integer and each internal node is one of '+', '−', '∗', or '/'.
@@ -18,8 +16,32 @@ For example, given the following tree:
 You should return 45, as it is (3 + 2) * (4 + 5).
 */
 
-fn problem_050() -> i64 {
-    0
+use crate::utils::bt_node::BtNode;
+
+fn problem_050(node: BtNode<char>) -> i64 {
+    match node.val {
+        '*' => {
+            let left = problem_050(*node.l.unwrap());
+            let right = problem_050(*node.r.unwrap());
+            left * right
+        }
+        '/' => {
+            let left = problem_050(*node.l.unwrap());
+            let right = problem_050(*node.r.unwrap());
+            left / right
+        }
+        '+' => {
+            let left = problem_050(*node.l.unwrap());
+            let right = problem_050(*node.r.unwrap());
+            left + right
+        }
+        '-' => {
+            let left = problem_050(*node.l.unwrap());
+            let right = problem_050(*node.r.unwrap());
+            left - right
+        }
+        x => x.to_string().parse().unwrap(),
+    }
 }
 
 #[cfg(test)]
@@ -27,8 +49,36 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_problem_050() {
-        assert_eq!(problem_050(), 1);
+        let root = BtNode {
+            val: '*',
+            l: Some(Box::new(BtNode {
+                val: '+',
+                l: Some(Box::new(BtNode {
+                    val: '3',
+                    l: None,
+                    r: None,
+                })),
+                r: Some(Box::new(BtNode {
+                    val: '2',
+                    l: None,
+                    r: None,
+                })),
+            })),
+            r: Some(Box::new(BtNode {
+                val: '+',
+                l: Some(Box::new(BtNode {
+                    val: '4',
+                    l: None,
+                    r: None,
+                })),
+                r: Some(Box::new(BtNode {
+                    val: '5',
+                    l: None,
+                    r: None,
+                })),
+            })),
+        };
+        assert_eq!(problem_050(root), 45);
     }
 }
