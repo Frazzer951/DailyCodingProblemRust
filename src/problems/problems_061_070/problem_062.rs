df086cc6119632d3a46fedfc608c4ec1f25aba84
Luke Eltiste <luke343279@gmail.com>
2022-02-21 13:08:33 -0800

    problem_062.rs

--- src/problems/problems_061_070/problem_062.rs
+++ src/problems/problems_061_070/problem_062.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* MEDIUM
 There is an N by M matrix of zeroes. Given N and M, write a function to count
 the number of ways of starting at the top-left corner and getting to the
@@ -14,8 +12,22 @@ ways to get to the bottom-right:
 Given a 5 by 5 matrix, there are 70 ways to get to the bottom-right.
 */
 
-fn problem_062() -> i64 {
-    0
+fn problem_062(n: usize, m: usize) -> i64 {
+    let mut arr = vec![vec![0; m]; n];
+    for i in arr.iter_mut().take(n) {
+        i[0] = 1;
+    }
+    for i in 0..m {
+        arr[0][i] = 1;
+    }
+
+    for i in 1..n {
+        for j in 1..m {
+            arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+        }
+    }
+
+    arr[n - 1][m - 1]
 }
 
 #[cfg(test)]
@@ -23,8 +35,7 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_problem_062() {
-        assert_eq!(problem_062(), 1);
+        assert_eq!(problem_062(5, 5), 70);
     }
 }
