fec6fa59a4cfcd420c58675b204b4b9400d4942f
Luke Eltiste <luke343279@gmail.com>
2022-02-21 14:31:47 -0800

    problem_343.rs

--- src/problems/problems_341_350/problem_343.rs
+++ src/problems/problems_341_350/problem_343.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* MEDIUM
 Given a binary search tree and a range [a, b] (inclusive), return the sum of the
 elements of the binary search tree within the range.
@@ -16,8 +14,24 @@ For example, given the following tree:
 and the range [4, 9], return 23 (5 + 4 + 6 + 8).
 */
 
-fn problem_343() -> i64 {
-    0
+use crate::utils::bt_node::BtNode;
+
+fn problem_343(root: BtNode<i64>, lower: i64, upper: i64) -> i64 {
+    let mut sum = 0;
+
+    if root.val >= lower && root.val <= upper {
+        sum += root.val;
+    }
+
+    if root.val >= lower && root.l.is_some() {
+        sum += problem_343(*root.l.unwrap(), lower, upper);
+    }
+
+    if root.val <= upper && root.r.is_some() {
+        sum += problem_343(*root.r.unwrap(), lower, upper);
+    }
+
+    sum
 }
 
 #[cfg(test)]
@@ -25,8 +39,37 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_problem_343() {
-        assert_eq!(problem_343(), 1);
+        let root = BtNode {
+            val: 5,
+            l: Some(Box::new(BtNode {
+                val: 3,
+                l: Some(Box::new(BtNode {
+                    val: 2,
+                    l: None,
+                    r: None,
+                })),
+                r: Some(Box::new(BtNode {
+                    val: 4,
+                    l: None,
+                    r: None,
+                })),
+            })),
+            r: Some(Box::new(BtNode {
+                val: 8,
+                l: Some(Box::new(BtNode {
+                    val: 6,
+                    l: None,
+                    r: None,
+                })),
+                r: Some(Box::new(BtNode {
+                    val: 10,
+                    l: None,
+                    r: None,
+                })),
+            })),
+        };
+
+        assert_eq!(problem_343(root, 4, 9), 23);
     }
 }
