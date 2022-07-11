dc047c66dd5e93589ecf8316926a00e8a0eb5969
Luke Eltiste <luke343279@gmail.com>
2022-07-11 12:02:35 -0700

    complete problem_036.rs

--- src/problems/problems_031_040/problem_036.rs
+++ src/problems/problems_031_040/problem_036.rs
@@ -1,20 +1,54 @@
-// NOT DONE
-
 /* MEDIUM
 Given the root to a binary search tree, find the second largest node in the
 tree.
 */
 
-//fn problem_036() -> i64 {
-//    0
-//}
+use crate::utils::bt_node::BtNode;
 
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//
-//    #[test]
-//    fn test_problem_036() {
-//        assert_eq!(problem_036(), 1);
-//    }
-//}
+fn second_largest_helper(root: &BtNode<i32>, c: &mut i32) -> Option<i32> {
+    if *c >= 2 {
+        return None;
+    }
+
+    if root.r.is_some() {
+        let result = second_largest_helper(root.r.as_ref().unwrap().as_ref(), c);
+        if result.is_some() { return result; }
+    }
+
+    *c += 1;
+
+    if *c == 2 {
+        return Some(root.val);
+    }
+
+    if root.l.is_some() {
+        let result = second_largest_helper(root.l.as_ref().unwrap().as_ref(), c);
+        if result.is_some() { return result; }
+    }
+
+    None
+}
+
+fn problem_036(node: BtNode<i32>) -> i32 {
+    second_largest_helper(&node, &mut 0).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_problem_036_1() {
+        assert_eq!(problem_036(BtNode::new_from_vec(vec![1, 2, 3, 4])), 3);
+    }
+
+    #[test]
+    fn test_problem_036_2() {
+        assert_eq!(problem_036(BtNode::new_from_vec(vec![4, 3, 2, 1])), 3);
+    }
+
+    #[test]
+    fn test_problem_036_3() {
+        assert_eq!(problem_036(BtNode::new_from_vec(vec![3, 2, 1, 4])), 3);
+    }
+}
