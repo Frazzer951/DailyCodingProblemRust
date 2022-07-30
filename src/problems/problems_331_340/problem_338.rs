9ceab0064f8f9e86fb7af38efa42f6ff075273e5
Frazzer951 <luke343279@gmail.com>
2022-07-29 20:44:26 -0700

    Add More tests to problem 338

--- src/problems/problems_331_340/problem_338.rs
+++ src/problems/problems_331_340/problem_338.rs
@@ -3,7 +3,7 @@ Given an integer n, find the next biggest integer with the same number of 1-bits
 on. For example, given the number 6 (0110 in binary), return 9 (1001).
 */
 
-fn problem_338(mut num: i64) -> i64 {
+fn next_biggest(mut num: i64) -> i64 {
     if num == 0 {
         return 0;
     }
@@ -31,7 +31,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_problem_338() {
-        assert_eq!(problem_338(6), 9);
+    fn test_next_biggest() {
+        assert_eq!(next_biggest(6), 9);
+        assert_eq!(next_biggest(0), 0);
     }
 }
