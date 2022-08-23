3c994a5274fe267f17a48db0c1384e52a0e619c0
Frazzer951 <luke343279@gmail.com>
2022-08-23 14:57:33 -0700

    Problem 57

--- src/problems/problems_051_060/problem_057.rs
+++ src/problems/problems_051_060/problem_057.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* MEDIUM
 Given a string s and an integer k, break up the string into multiple lines such
 that each line has a length of k or less. You must break it up so that words
@@ -14,16 +12,49 @@ k = 10, you should return: ["the quick", "brown fox", "jumps over", "the lazy",
 "dog"]. No string in the list has a length of more than 10.
 */
 
-//fn problem_057() -> i64 {
-//    0
-//}
+fn problem_057(s: String, k: usize) -> Vec<String> {
+    let mut words: Vec<&str> = s.split(' ').collect();
+    let mut output = vec![];
 
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//
-//    #[test]
-//    fn test_problem_057() {
-//        assert_eq!(problem_057(), 1);
-//    }
-//}
+    while !words.is_empty() {
+        let mut cur = String::new();
+        let mut cur_len = 0;
+        while cur.len() < k && !words.is_empty() {
+            let next_word = words[0];
+            if cur_len == 0 {
+                cur.push_str(next_word);
+                cur_len += next_word.len();
+                words.remove(0);
+            } else if cur_len + next_word.len() < k {
+                cur.push(' ');
+                cur.push_str(next_word);
+                cur_len += next_word.len() + 1;
+                words.remove(0);
+            } else {
+                break;
+            }
+        }
+        output.push(cur.clone());
+    }
+
+    output
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_problem_057() {
+        assert_eq!(
+            problem_057("the quick brown fox jumps over the lazy dog".to_string(), 10),
+            vec![
+                "the quick".to_string(),
+                "brown fox".to_string(),
+                "jumps over".to_string(),
+                "the lazy".to_string(),
+                "dog".to_string()
+            ]
+        );
+    }
+}
