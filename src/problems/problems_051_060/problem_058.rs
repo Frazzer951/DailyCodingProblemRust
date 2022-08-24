6253653ada4cd624d0b159565abd1925dfc08f6e
Frazzer951 <luke343279@gmail.com>
2022-08-23 18:05:59 -0700

    Problem 58

--- src/problems/problems_051_060/problem_058.rs
+++ src/problems/problems_051_060/problem_058.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* MEDIUM
 An sorted array of integers was rotated an unknown number of times.
 
@@ -12,16 +10,64 @@ For example, given the array [13, 18, 25, 2, 8, 10] and the element 8, return 4
 You can assume all the integers in the array are unique.
 */
 
-//fn problem_058() -> i64 {
-//    0
-//}
+fn shifted_array_search(arr: Vec<i32>, num: i32) -> Option<usize> {
+    let mut i = arr.len() / 2;
+    let mut dist = i / 2;
 
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//
-//    #[test]
-//    fn test_problem_058() {
-//        assert_eq!(problem_058(), 1);
-//    }
-//}
+    loop {
+        if arr[0] > arr[i] && arr[i - 1] > arr[i] {
+            break;
+        }
+        if dist == 0 {
+            break;
+        }
+        if arr[0] <= arr[i] {
+            i += dist;
+        } else if arr[i - 1] <= arr[i] {
+            i -= dist;
+        } else {
+            break;
+        }
+        dist /= 2;
+    }
+
+    let mut low = i;
+    let mut high = i - 1;
+    dist = arr.len() / 2;
+
+    loop {
+        if dist == 0 {
+            return None;
+        }
+
+        let guess_index = (low + dist) % arr.len();
+        let guess = arr[guess_index];
+        if guess == num {
+            return Some(guess_index);
+        }
+
+        if guess < num {
+            low = (low + dist) % arr.len();
+        }
+        if guess > num {
+            high = (arr.len() + high - dist) % arr.len();
+        }
+
+        dist /= 2;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_problem_058() {
+        assert_eq!(shifted_array_search(vec![13, 18, 25, 2, 8, 10], 8), Some(4));
+    }
+
+    #[test]
+    fn test_problem_058_missing() {
+        assert_eq!(shifted_array_search(vec![13, 18, 25, 2, 8, 10], 0), None);
+    }
+}
