3f8c15d76a3e47dcd739f44581af87f66c755106
Frazzer951 <luke343279@gmail.com>
2022-09-08 15:31:23 -0700

    problem 77

--- src/problems/problems_071_080/problem_077.rs
+++ src/problems/problems_071_080/problem_077.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* EASY
 Given a list of possibly overlapping intervals, return a new list of intervals
 where all overlapping intervals have been merged.
@@ -9,17 +7,50 @@ The input list is not necessarily ordered in any way.
 For example, given [(1, 3), (5, 8), (4, 10), (20, 25)], you should return [(1,
 3), (4, 10), (20, 25)].
 */
+s
+use std::collections::vec_deque::VecDeque;
 
-//fn problem_077() -> i64 {
-//    0
-//}
+fn overlaps(i1: &(i32, i32), i2: &(i32, i32)) -> bool {
+    if i1.0 >= i2.0 && i1.0 <= i2.1 {
+        return true;
+    }
+    if i2.0 >= i1.0 && i2.0 <= i1.1 {
+        return true;
+    }
 
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//
-//    #[test]
-//    fn test_problem_077() {
-//        assert_eq!(problem_077(), 1);
-//    }
-//}
+    false
+}
+
+fn merge_intervals(intervals: Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+    let mut merged = vec![];
+    let mut intervals = VecDeque::from(intervals);
+
+    while !intervals.is_empty() {
+        let mut cur_interval = intervals.pop_front().unwrap();
+        intervals.retain(|interval| {
+            if overlaps(&cur_interval, interval) {
+                cur_interval.0 = cur_interval.0.min(interval.0);
+                cur_interval.1 = cur_interval.1.max(interval.1);
+                return false;
+            }
+
+            true
+        });
+        merged.push(cur_interval);
+    }
+
+    merged
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_merge_intervals() {
+        assert_eq!(
+            merge_intervals(vec![(1, 3), (5, 8), (4, 10), (20, 25)]),
+            vec![(1, 3), (4, 10), (20, 25)]
+        );
+    }
+}
