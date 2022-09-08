e55a6262d929b9e9139210455941aca7b2e886fd
Frazzer951 <luke343279@gmail.com>
2022-09-08 16:03:36 -0700

    problem 80

--- src/problems/problems_071_080/problem_080.rs
+++ src/problems/problems_071_080/problem_080.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* EASY
 Given the root of a binary tree, return a deepest node. For example, in the
 following tree, return d.
@@ -11,16 +9,52 @@ following tree, return d.
 d
 */
 
-//fn problem_080() -> i64 {
-//    0
-//}
+use crate::utils::bt_node::BtNode;
 
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//
-//    #[test]
-//    fn test_problem_080() {
-//        assert_eq!(problem_080(), 1);
-//    }
-//}
+fn deepest_node_helper(root: &BtNode<i32>, depth: u32) -> (i32, u32) {
+    if root.l.is_none() && root.r.is_none() {
+        return (root.val, depth);
+    }
+    if root.l.is_some() && root.r.is_some() {
+        let left = deepest_node_helper(root.l.as_ref().unwrap().as_ref(), depth + 1);
+        let right = deepest_node_helper(root.r.as_ref().unwrap().as_ref(), depth + 1);
+        if left.1 >= right.1 {
+            return left;
+        } else {
+            return right;
+        }
+    }
+    if root.l.is_some() {
+        return deepest_node_helper(root.l.as_ref().unwrap().as_ref(), depth + 1);
+    }
+    deepest_node_helper(root.r.as_ref().unwrap().as_ref(), depth + 1)
+}
+
+fn deepest_node(root: &BtNode<i32>) -> i32 { deepest_node_helper(root, 0).0 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_deepest_node() {
+        let root = BtNode {
+            val: 1,
+            l:   Some(Box::new(BtNode {
+                val: 2,
+                l:   Some(Box::new(BtNode {
+                    val: 4,
+                    l:   None,
+                    r:   None,
+                })),
+                r:   None,
+            })),
+            r:   Some(Box::new(BtNode {
+                val: 3,
+                l:   None,
+                r:   None,
+            })),
+        };
+        assert_eq!(deepest_node(&root), 4);
+    }
+}
