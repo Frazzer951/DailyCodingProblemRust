0b07bad6f4f7fe6c33d714dfc603df71e2143554
Luke Eltiste <luke343279@gmail.com>
2022-11-14 18:00:01 -0800

    problem 81

--- src/problems/problems_081_090/problem_081.rs
+++ src/problems/problems_081_090/problem_081.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* EASY
 Given a mapping of digits to letters (as in a phone number), and a digit string,
 return all possible letters the number could represent. You can assume each
@@ -9,16 +7,41 @@ For example if {“2”: [“a”, “b”, “c”], 3: [“d”, “e”, “f
 return [“ad”, “ae”, “af”, “bd”, “be”, “bf”, “cd”, “ce”, “cf"].
 */
 
-//fn problem_081() -> i64 {
-//    0
-//}
+use std::collections::HashMap;
 
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//
-//    #[test]
-//    fn test_problem_081() {
-//        assert_eq!(problem_081(), 1);
-//    }
-//}
+fn possible_strings(mapping: &HashMap<char, Vec<char>>, number: String) -> Vec<String> {
+    let n = number.len();
+
+    let mut possibilities: Vec<String> = vec![];
+
+    for index in (0..n).rev() {
+        let n = number.chars().nth(index).unwrap();
+        let mut new_pos = vec![];
+        for c in mapping.get(&n).unwrap() {
+            if possibilities.is_empty() {
+                new_pos.push(c.to_string())
+            } else {
+                for p in &possibilities {
+                    new_pos.push(c.to_string() + p);
+                }
+            }
+        }
+        possibilities = new_pos;
+    }
+
+    possibilities
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_possible_strings() {
+        let map = HashMap::from([('2', vec!['a', 'b', 'c']), ('3', vec!['d', 'e', 'f'])]);
+        assert_eq!(
+            possible_strings(&map, "23".to_string()),
+            vec!["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+        );
+    }
+}
