5418fddc27b8106234a76ef847316833c455a534
Luke Eltiste <luke343279@gmail.com>
2022-11-15 12:42:24 -0800

    problem 86

--- src/problems/problems_081_090/problem_086.rs
+++ src/problems/problems_081_090/problem_086.rs
@@ -1,5 +1,3 @@
-// NOT DONE
-
 /* MEDIUM
 Given a string of parentheses, write a function to compute the minimum number of
 parentheses to be removed to make the string valid (i.e. each open parenthesis
@@ -9,16 +7,28 @@ For example, given the string "()())()", you should return 1. Given the string
 ")(", you should return 2, since we must remove all of them.
 */
 
-//fn problem_086() -> i64 {
-//    0
-//}
+fn problem_086(parens: String) -> i32 {
+    let mut open = 0;
+    let mut count = 0;
+    for c in parens.chars() {
+        if c == '(' {
+            open += 1;
+        } else if open > 0 {
+            open -= 1;
+        } else {
+            count += 1;
+        }
+    }
+    count + open
+}
 
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//
-//    #[test]
-//    fn test_problem_086() {
-//        assert_eq!(problem_086(), 1);
-//    }
-//}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_problem_086() {
+        assert_eq!(problem_086("()())()".to_owned()), 1);
+        assert_eq!(problem_086(")(".to_owned()), 2);
+    }
+}
