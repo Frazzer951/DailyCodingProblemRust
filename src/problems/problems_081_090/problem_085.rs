554800c228607c16f672245c8735f602ee36d816
Luke Eltiste <luke343279@gmail.com>
2022-11-15 12:35:04 -0800

    Problem 85

--- src/problems/problems_081_090/problem_085.rs
+++ src/problems/problems_081_090/problem_085.rs
@@ -1,20 +1,19 @@
-// NOT DONE
-
 /* MEDIUM
 Given three 32-bit integers x, y, and b, return x if b is 1 and y if b is 0,
 using only mathematical or bit operations. You can assume b can only be 1 or 0.
 */
 
-//fn problem_085() -> i64 {
-//    0
-//}
+fn problem_085(x: i32, y: i32, b: i32) -> i32 {
+    (x * b) | (y * (1 - b))
+}
 
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//
-//    #[test]
-//    fn test_problem_085() {
-//        assert_eq!(problem_085(), 1);
-//    }
-//}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_problem_085() {
+        assert_eq!(problem_085(1, 2, 1), 1);
+        assert_eq!(problem_085(1, 2, 0), 2);
+    }
+}
